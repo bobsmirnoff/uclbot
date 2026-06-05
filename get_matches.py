37e551b2952f9c5c4b94c45c0080e503317f0f8d
Boris Smirnov <boris.v.smirnov@gmail.com>
2026-06-05 15:28:17 +0200

    get_matches update
    Refactor get_matches.py to use requests library for API calls and improve team name trimming.

--- get_matches.py
+++ get_matches.py
@@ -1,45 +1,47 @@
 import http.client
 import json
 import os
+import requests
 from datetime import datetime
 import string
-
-# with open('matches.json', 'w') as outfile:
-   # json.dump(response, outfile)
+from competition import comp
 
 
 def trim(team_name):
+    if team_name == '' or not team_name:
+        return ''
     return team_name.replace('AFC', '') \
                .replace('FC', '') \
                .replace('FK', '') \
                .replace('TC', '') \
+               .replace('CF', '') \
                .replace('BV', '') \
                .replace('KV', '') \
+               .replace('SSC', '') \
                .replace('SS', '') \
+               .replace('SK', '') \
                .replace('09', '') \
                .replace('1901', '') \
+               .replace('1.', '') \
                .replace('   ', '') \
                .replace('  ', ' ') \
                .strip()
 
 
 def get_matches(date_str):
-    connection = http.client.HTTPConnection('api.football-data.org')
-    headers = { 'X-Auth-Token': os.getenv('FOOTBALL_DATA_TOKEN', '') }
+    url = "https://api.football-data.org"
+    headers = {"X-Auth-Token": os.getenv('FOOTBALL_DATA_TOKEN', '')}
 
-    print('ready to make request')
-    connection.request('GET', '/v2/competitions/CL/matches?status=SCHEDULED', None, headers)
-    response = connection.getresponse()
-    raw_response = response.read().decode()
-    print('got response ' + str(response.status))
+    session = requests.Session()
+    session.headers.update(headers)
 
-    response = json.loads(raw_response)
-
-    groups = dict(('Group ' + g, []) for g in string.ascii_uppercase[:8])
+    response = session.get(url + '/v4/competitions/%s/matches?status=SCHEDULED' % comp['name'], allow_redirects=True).json()
+    groups = dict(('GROUP_' + g, []) for g in string.ascii_uppercase[:8])
     play_off = []
 
     for match in response['matches']:
-        if match['utcDate'].startswith(date_str):
+        if match['utcDate'].startswith(date_str) and match['homeTeam']['name'] and match['awayTeam']['name']:
+            print(match)
             title = trim(match['homeTeam']['name']) + " vs " + trim(match['awayTeam']['name'])
             if match.get('group'):
                 groups[match['group']].append(title)
@@ -49,7 +51,7 @@ def get_matches(date_str):
     matches = []
     for g in sorted(groups):
         if len(groups[g]) > 0:
-            matches.append(g)
+            matches.append(g.replace('_', ' '))
             matches += groups[g]
             matches.append('')
     for m in play_off:
@@ -59,18 +61,20 @@ def get_matches(date_str):
 
 
 def get_matches_struct(date_str):
-    connection = http.client.HTTPConnection('api.football-data.org')
-    headers = { 'X-Auth-Token': os.getenv('FOOTBALL_DATA_TOKEN', '') }
-    connection.request('GET', '/v2/competitions/CL/matches?status=SCHEDULED', None, headers )
-    raw_response = connection.getresponse().read().decode()
-    response = json.loads(raw_response)
+    url = "https://api.football-data.org"
+    headers = {"X-Auth-Token": os.getenv('FOOTBALL_DATA_TOKEN', '')}
+
+    session = requests.Session()
+    session.headers.update(headers)
+
+    response = session.get(url + '/v4/competitions/%s/matches?status=SCHEDULED' % comp['name'], allow_redirects=True).json()
 
     matches = []
     for m in response['matches']:
-        if m['utcDate'].startswith(date_str):
+        if m['utcDate'].startswith(date_str) and m['homeTeam']['name'] and m['awayTeam']['name']:
             matches.append({'id': m['id'], 'start_time': datetime.strptime(m['utcDate'], '%Y-%m-%dT%H:%M:%SZ').strftime("%Y-%m-%d %H:%M:%S"),
-                                'home_team': {'id': m['homeTeam']['id'], 'name': m['homeTeam']['name']},
-                                'away_team': {'id': m['awayTeam']['id'], 'name': m['awayTeam']['name']}
+                                'home_team': {'id': m['homeTeam']['id'], 'name': trim(m['homeTeam']['name'])},
+                                'away_team': {'id': m['awayTeam']['id'], 'name': trim(m['awayTeam']['name'])}
             })
     return matches
 
@@ -80,22 +84,26 @@ def post_matches(date_str):
     matches = get_matches_struct(date_str)
 
     for m in matches:
-        data = '{"ext_id": "%s", "datetime": "%s", "comp_id": 25, "comp1": {"ext_id": %s, "name": "%s"}, "comp2": {"ext_id": %s, "name": "%s"}}' \
-            % (m['id'], m['start_time'], m['home_team']['id'], m['home_team']['name'], m['away_team']['id'], m['away_team']['name'])
+        data = '{"ext_id": "%s", "datetime": "%s", "comp_id": %s, "comp1": {"ext_id": %s, "name": "%s"}, "comp2": {"ext_id": %s, "name": "%s"}}' \
+            % (m['id'], m['start_time'], comp['id'], m['home_team']['id'], m['home_team']['name'], m['away_team']['id'], m['away_team']['name'])
         connection.request('POST', '/api_proc.php?access_key=' + os.getenv('PIPEINPIPE_ACCESS_KEY', '')  + '&action=import_ext_match', data)
         print(connection.getresponse().read().decode())
 
 
-def get_teams():
-    connection = http.client.HTTPConnection('api.football-data.org')
-    headers = { 'X-Auth-Token': os.getenv('FOOTBALL_DATA_TOKEN', '') }
-    connection.request('GET', '/v2/competitions/CL/teams', None, headers)
-    raw_response = connection.getresponse().read().decode()
-    response = json.loads(raw_response)
+def print_teams():
+    url = "https://api.football-data.org"
+    headers = {"X-Auth-Token": os.getenv('FOOTBALL_DATA_TOKEN', '')}
+
+    session = requests.Session()
+    session.headers.update(headers)
+
+    response = session.get(url + '/v4/competitions/%s/teams' % comp['name'], allow_redirects=True).json()
 
     for t in response["teams"]:
         print(t["name"] + " - " + str(t["id"]))
-    return response
 
 
-# post_matches("2020-11-03")
+#post_matches("2026-06-12")
+#print(get_matches("2026-06-12"))
+#print(get_matches_struct("2026-06-12"))
+#print_teams()
